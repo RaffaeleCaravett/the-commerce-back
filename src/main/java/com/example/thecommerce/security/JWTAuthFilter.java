221ceaad9d2065f54d9d94ccb925d7c406919e2b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-17 09:27:44 +0100

    should not filter everything

--- src/main/java/com/example/thecommerce/security/JWTAuthFilter.java
+++ src/main/java/com/example/thecommerce/security/JWTAuthFilter.java
@@ -76,7 +76,7 @@ public class JWTAuthFilter extends OncePerRequestFilter {
         // Ad es tutte le richieste al controller /auth/** non devono essere filtrate
         String pathWithArguments = request.getServletPath() + request.getQueryString();
 
-        List<String> excludedPaths = Arrays.asList("/api/arguments", "/auth/","/api/continents","/api/nation","/api/visit");
+        List<String> excludedPaths = Arrays.asList("/*");
 
         return excludedPaths.stream().anyMatch(pathWithArguments::startsWith);
     }
